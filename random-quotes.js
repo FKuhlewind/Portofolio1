61037f993b301539828d77a27edc2170f143b27f
FKuhlewind <florian.kuhlewind@gmail.com>
2014-03-16 16:10:20 +0100

    Update random-quotes.js

--- random-quotes.js
+++ random-quotes.js
@@ -1,6 +1,14 @@
 $(document).ready(function() { 
     
+    myDataRef = new Firebase('https://ast-quotes.firebaseio.com/');
     
-    
+    function changeText() {
+ 
+        var random = 1 + Math.floor(Math.random() * 3);
+        console.log(random);
+        
+        };
+        
+    setInterval(changeText, 1000);
     
 });  
