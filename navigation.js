d63ebd96d1202d47834fb84973df7c235943c25a
FKuhlewind <florian.kuhlewind@gmail.com>
2014-03-16 21:54:37 +0100

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -12,6 +12,20 @@ $(document).ready(function() {
         parent2.insertBefore(elm1, next2);
         };
     $("#swap_button").on("click", function () {
+    	
+    	
+    	function swapElements(elm1, elm2) {
+        var parent1, next1,
+        parent2, next2;
+        parent1 = elm1.parentNode;
+        next1   = elm1.nextSibling;
+        parent2 = elm2.parentNode;
+        next2   = elm2.nextSibling;
+        parent1.insertBefore(elm2, next1);
+        parent2.insertBefore(elm1, next2);
+        };
+    	
+    	
         swapElements($(".choose .code")[0], $(".choose .assignment")[0]);
         });
    
