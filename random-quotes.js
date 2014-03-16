6b91a21d43d75f594df4af166a43cd35e2d1edaa
FKuhlewind <florian.kuhlewind@gmail.com>
2014-03-16 16:23:01 +0100

    Update random-quotes.js

--- random-quotes.js
+++ random-quotes.js
@@ -1,14 +1,21 @@
 $(document).ready(function() { 
     
-    //myDataRef = new Firebase('https://ast-quotes.firebaseio.com/');
+    myDataRef = new Firebase('https://ast-quotes.firebaseio.com/');
     
     function changeText() {
  
-        var random = 1 + Math.floor(Math.random() * 3);
-        console.log(random);
+        d3.json('https://ast-quotes.firebaseio.com/.json', function(data) {
+		    quotes = data;
+            var random = 1 + Math.floor(Math.random() * 3);
+            
+            console.log(quotes);
+            
+            });
+        
+        //console.log(random);
         
         };
         
-    setInterval(changeText, 10000);
+    setInterval(changeText, 5000);
     
 });  
