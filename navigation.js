34acf765b077099b0ffa7f905e296b7511b37f5e
FKuhlewind <florian.kuhlewind@gmail.com>
2014-03-16 19:00:01 +0100

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,23 +1,21 @@
 $(document).ready(function() { 
     
+    //enable swapping
     function swapElements(elm1, elm2) {
         var parent1, next1,
         parent2, next2;
-
         parent1 = elm1.parentNode;
         next1   = elm1.nextSibling;
         parent2 = elm2.parentNode;
         next2   = elm2.nextSibling;
-
         parent1.insertBefore(elm2, next1);
         parent2.insertBefore(elm1, next2);
         };
-    
     $("#swap_button").on("click", function () {
         swapElements($(".code")[0], $(".assignment")[0]);
         });
    
-    
+    //enable resizing
     $("#size_button").on("click", function () {
         $( ".code" ).animate({
             height: $(".assignment").css("height")
@@ -27,4 +25,39 @@ $(document).ready(function() {
             }, 1500 );    
         });
         
+    //enable navigation    
+    $('ul.tabs').each(function(){
+
+    // Fuer jeden Satz Tabs wollen wir verfolgen welcher
+    // Tab aktiv ist und der ihm zugeordnete Inhalt
+    var $active, $content, $links = $(this).find('a');
+
+    // Der erste Link ist der zu Anfang akitve Tab
+    $active = $links.first().addClass('active');
+    $content = $($active.attr('href'));
+
+    // Verstecke den restlichen Inhalt
+    $links.not(':first').each(function () {
+        $($(this).attr('href')).hide();
+    });
+
+    // Binde den click event handler ein
+    $(this).on('click', 'a', function(e){
+
+        // Mache den alten Tab inaktiv
+        $active.removeClass('active');
+        $content.hide();
+
+        // Aktualisiere die Variablen mit dem neuen Link und Inhalt
+        $active = $(this);
+        $content = $($(this).attr('href'));
+
+        // Setze den Tab aktiv
+        $active.addClass('active');
+        $content.show();
+
+        // Verhindere die Anker standard click Aktion
+        e.preventDefault();
+    });
+	});
 });  
