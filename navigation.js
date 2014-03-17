0ba128bcad72aadbbb61fc5155e0bdeec13de8a8
FKuhlewind <florian.kuhlewind@gmail.com>
2014-03-17 19:28:21 +0100

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -33,10 +33,10 @@ $(document).ready(function() {
     	
     	sel = ($(this).attr("value"));
     	
-    	$(".assignment iframe").fadeTo(400, 0).remove(); 
+    	$(".assignment iframe").fadeTo(1400, 0).remove(); 
     	$(".assignment").append("<iframe src='page-"+sel+".html'></iframe>").fadeTo(1,0).fadeTo(1000,1);
     	
-    	$(".code iframe").fadeTo(400, 0).remove(); 
+    	$(".code iframe").fadeTo(1400, 0).remove(); 
     	$(".code").append("<iframe src='code-"+sel+".html'></iframe>").fadeTo(1,0).fadeTo(1000,1);
     	
     	
