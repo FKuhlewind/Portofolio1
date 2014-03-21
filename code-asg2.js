7331f65244aa52a20662abfa30b94b44e092159c
FKuhlewind <florian.kuhlewind@gmail.com>
2014-03-21 20:53:12 +0100

    Update code-asg2.js

--- code-asg2.js
+++ code-asg2.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 d3.json('https://student-data.firebaseio.com/.json', function(data) {
 	studentData = data;
 	//studentData = $.grep(studentData ,function(n){ return(n) });
+	studentData = studentData.filter(function(e){return e}); 
 
     	var studentTemplateScript = $("#students-template").html();
 
