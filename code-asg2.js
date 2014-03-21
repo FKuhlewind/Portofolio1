b388cf91bdd1e2b5a75150d810d645130e8c8e84
FKuhlewind <florian.kuhlewind@gmail.com>
2014-03-21 20:55:00 +0100

    Update code-asg2.js

--- code-asg2.js
+++ code-asg2.js
@@ -1,9 +1,8 @@
 $(document).ready(function() { 
 
 d3.json('https://student-data.firebaseio.com/.json', function(data) {
-	studentData = data;
-	//studentData = $.grep(studentData ,function(n){ return(n) });
-	studentData.students = studentData.students.filter(function(e){return e}); 
+	//studentData = data;
+	studentData.students = data.students.filter(function(e){return e}); 
 
     	var studentTemplateScript = $("#students-template").html();
 
