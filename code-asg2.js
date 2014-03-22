c36471134d855aa0c5fb0ff98b198f240b701a37
FKuhlewind <florian.kuhlewind@gmail.com>
2014-03-22 17:29:53 +0100

    Update code-asg2.js

--- code-asg2.js
+++ code-asg2.js
@@ -47,11 +47,7 @@ d3.json('https://student-data.firebaseio.com/.json', function(data) {
 		
 		//alert ( $("#firstin").val()+" "+$("#lastin").val()  );
 		
-		// Generate a reference to a new location with push
-		//var newPushRef = listRef.push();
-
-		// Set some data to the generated location
-		//newPushRef.set("first": $("#firstin").val(), "last": $("#lastin").val()});
+		myDataRef.child(appNum).child("first").set( $("#firstin").val() );
 		
 		
 		});
