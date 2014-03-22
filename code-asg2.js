882c24742952f07f0d8a5480d4a394557dade9f7
FKuhlewind <florian.kuhlewind@gmail.com>
2014-03-22 19:23:20 +0100

    Update code-asg2.js

--- code-asg2.js
+++ code-asg2.js
@@ -25,15 +25,7 @@ d3.json('https://student-data.firebaseio.com/.json', function(data) {
 
 	// try to set new firebase here
 	
-	for ( var i = 0, l = appNum; i < l; i++ ) {
-	//for ( var i = 0, l = studentData.students.length; i < l; i++ ) {
-		
-		myDataRef.child("students").child(i).child("first").set( studentData.students[i].first  );
-		myDataRef.child("students").child(i).child("last").set( studentData.students[i].last  );
-		myDataRef.child("students").child(i).child("uvaID").set( studentData.students[i].uvaID  );
-		myDataRef.child("students").child(i).child("email").set( studentData.students[i].email  );
-		};
-	myDataRef.child("students").child(studentData.students.length-1).set( null );
+	
 	
 	
 
@@ -73,6 +65,17 @@ d3.json('https://student-data.firebaseio.com/.json', function(data) {
 		
 		myDataRef.child("students").child( $(this).val() ).set(null);
 		alert ("Student removed.");
+		
+		for ( var i = 0, l = appNum; i < l; i++ ) {
+		//for ( var i = 0, l = studentData.students.length; i < l; i++ ) {
+		
+			myDataRef.child("students").child(i).child("first").set( studentData.students[i].first  );
+			myDataRef.child("students").child(i).child("last").set( studentData.students[i].last  );
+			myDataRef.child("students").child(i).child("uvaID").set( studentData.students[i].uvaID  );
+			myDataRef.child("students").child(i).child("email").set( studentData.students[i].email  );
+			};
+		myDataRef.child("students").child(studentData.students.length-1).set( null );
+		
 		window.location.reload();
 		});
 
