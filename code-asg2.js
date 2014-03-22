4d8b56ac4d4f496d9ffcb871cc3acf484a27209e
FKuhlewind <florian.kuhlewind@gmail.com>
2014-03-22 17:04:16 +0100

    Update code-asg2.js

--- code-asg2.js
+++ code-asg2.js
@@ -9,7 +9,7 @@ myDataRef = new Firebase('https://student-data.firebaseio.com/');
 d3.json('https://student-data.firebaseio.com/.json', function(data) {
 	studentData = data;
 	
-	myDataRef.set(studentData);
+	myDataRef.set(studentData.students);
 	
 	studentData.students = studentData.students.filter(function(e){return e}); 
 
