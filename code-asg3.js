532700fd83fd014f03adb1d85ef4c764fc6b66db
FKuhlewind <florian.kuhlewind@gmail.com>
2014-03-23 16:46:00 +0100

    Update code-asg3.js

--- code-asg3.js
+++ code-asg3.js
@@ -66,7 +66,7 @@ createSVG = function () {
 	//create X axis
 	mySVG.append("g")
     		.attr("transform", "translate(0," + (height - padding) + ")")
-    		.style({ 'stroke': 'Black', 'fill': 'none', 'stroke-width': '1px'})
+    		.style({ 'stroke': 'Black', 'fill': 'none', /*'stroke-width': '1px'*/})
     		.call(xAxis);
 	};
 
