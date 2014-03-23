aef332525ce8a5cb06b7feeb6d1e72b5b1cd0067
FKuhlewind <florian.kuhlewind@gmail.com>
2014-03-23 14:07:29 +0100

    Update code-asg2.js

--- code-asg2.js
+++ code-asg2.js
@@ -67,7 +67,7 @@ d3.json('https://student-data.firebaseio.com/.json', function(data) {
 		//$("tr#stud"+$(this).val()+":nth-child(1)").replaceWith("<td>Yo Hey</td>");
 		
 		$("tr#stud"+vl).children(".inf1")
-			.replaceWith("<td><input type='text' value='Anouk'/></td>");
+			.replaceWith("<td><input type='text' value='studentData.students[1].first'/></td>");
 		
 		//alert ("Edit Number "+ $(this).val() );
 		 
