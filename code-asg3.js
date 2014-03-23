10d248d19452814e6c5c9a4a9fe21a8950e4093a
FKuhlewind <florian.kuhlewind@gmail.com>
2014-03-23 17:36:58 +0100

    Update code-asg3.js

--- code-asg3.js
+++ code-asg3.js
@@ -8,8 +8,7 @@ createData = function () {
 	var eventsTemplate = {
     		"events|5-20": [{
       		"name|1-2": "@LOREM ",
-      		//"rating|1-5": 0,
-      		"rating|"+rFrom+"-"+rTo: 0,
+      		"rating|1-7": 0,
       		"ficticious|0-1": true, }]
   		};
   	var events = $.mockJSON.generateFromTemplate(eventsTemplate);
