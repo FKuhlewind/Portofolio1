3d5ab74ebcdd8776f44306dc031a2ee05083c1d7
FKuhlewind <florian.kuhlewind@gmail.com>
2014-03-26 23:46:36 +0100

    Update code-asg1.js

--- code-asg1.js
+++ code-asg1.js
@@ -3,9 +3,10 @@ $(document).ready(function() {
 mainP = window.parent.document;
 
 $("#yellC").on("click", function () {
-    $(mainP).find(".author").replaceWith("Suppa");
+    //$(mainP).find(".author").replaceWith("Suppa");
     
-    
+    $(mainP).find(".qoute, .green").css({'background-color': '#8f9d45'});
+
     });
   
 
