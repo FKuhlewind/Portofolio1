c83d181202ca287abd8b17e230cf68b902a77d6d
FKuhlewind <florian.kuhlewind@gmail.com>
2014-03-26 23:33:15 +0100

    Update code-asg1.js

--- code-asg1.js
+++ code-asg1.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-mainp = window.parent.document;
+mainP = window.parent.document;
 
 $("#yellC").on("click", function () {
     $(mainP).find(".author").replaceWith("Suppa");
