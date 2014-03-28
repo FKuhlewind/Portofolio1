2603692a03fbf3aa7cdce366f1d18c3e3ad2804e
FKuhlewind <florian.kuhlewind@gmail.com>
2014-03-28 21:22:15 +0100

    Update code-asg1.js

--- code-asg1.js
+++ code-asg1.js
@@ -25,7 +25,7 @@ $("#colorScheme").on("click", ".col", function(e) {
             colN = $(this).data('value');
 
             $(mainP).find(toColor[0]).css({'background-color':colG[colN][0]+'1)', 'fill':colG[colN][0]+'1)', 'color':colG[colN][1]+'1)'});
-            $(mainP).find(toColor[1]+'1)').css({'background-color':colG[colN][2]+'1)'});
+            $(mainP).find(toColor[1]).css({'background-color':colG[colN][2]+'1)'});
             $(mainP).find(".nav-in ul li a").css({'border':'2px solid '+colG[colN][2]+'1)'});
             
             //change later!!!
