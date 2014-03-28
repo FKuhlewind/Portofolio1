ca67cddb4d8cbe41e24d146f19647c0ef9457de5
FKuhlewind <florian.kuhlewind@gmail.com>
2014-03-28 22:04:05 +0100

    Update code-asg1.js

--- code-asg1.js
+++ code-asg1.js
@@ -47,28 +47,4 @@ $("#fontScheme").on("click", ".col", function(e) {
             $('body, #onRight').css({'font-family':myFont});
             
             });
-
-
-
-$("#yellC").on("click", function () {
-    //$(mainP).find(".author").replaceWith("Suppa");
-    
-    $(mainP).find(".quote, .green, .text, .author, .wrapper").css({'background-color': 'yellow', 'fill': 'yellow', 'color': 'grey'});
-    $(mainP).find(".main, iframe::-webkit-scrollbar-track").css({'background-color': 'gray'});
-    });
-  
-$("#techF").on("click", function () {
-    
-    $(mainP).find(".text, .author, .nav-in ul li a, .copy p").css({'font-family': "Lucida Console" });
-    });
-
-$("#oceanB").on("click", function () {
-
-    $(mainP).find(".wrapper").css({'background-image':'url(http://www.earthtimes.org/nsimages/files/ocean-acidification_23412.jpg)',
-                                    'background-size':'100%'});
-    
-    
-
-    });
-
 });
