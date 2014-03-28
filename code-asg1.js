17e92fd790c78f2712fc40816140f19355f85e69
FKuhlewind <florian.kuhlewind@gmail.com>
2014-03-28 21:20:42 +0100

    Update code-asg1.js

--- code-asg1.js
+++ code-asg1.js
@@ -29,8 +29,8 @@ $("#colorScheme").on("click", ".col", function(e) {
             $(mainP).find(".nav-in ul li a").css({'border':'2px solid '+colG[colN][2]+'1)'});
             
             //change later!!!
-            $("#styleTable").css({'background-color':'rgba(88, 88, 88, 0.2)'});
-            $("#styleTable div").css({'background-color':'rgba(88, 88, 88, 0.3)'});
+            $("#styleTable").css({'background-color':colG[colN][0]+'0.2)'});
+            $("#styleTable div").css({'background-color':colG[colN][0]+'0.3)'});
             
             });
 
