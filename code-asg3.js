abec0e8fcefbedb3a8aedc1ebdabbcc38048c0c9
FKuhlewind <florian.kuhlewind@gmail.com>
2014-03-28 22:53:32 +0100

    Update code-asg3.js

--- code-asg3.js
+++ code-asg3.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
 // check localStorage and choose font accordingly
 $('body, #onRight').css({'font-family':localStorage.myFont});
 $("#background").css({'background-color':localStorage.myColor+'0.2)'});
-//$("svg").css({'background-color':localStorage.myColor+'0.3)'});
 
 eventsTemplate1 = {
     		"events|5-20":[{
