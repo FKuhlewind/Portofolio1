7e15ffab0436f97bcdbbed8ad84d77aad4c1b832
FKuhlewind <florian.kuhlewind@gmail.com>
2014-03-28 21:56:24 +0100

    Update code-asg1.js

--- code-asg1.js
+++ code-asg1.js
@@ -15,7 +15,7 @@ colG = [ ["rgba(88, 88, 88,", "rgba(193, 225, 166,", "rgba(17, 140, 78,"],
 toColor = [ ".quote, .green, .text, .author",
             ".nav, .footer" ];
 
-fontG = [ 'Monaco',
+fontG = [ '"Monaco"',
           '"Lucida Bright"',
           '"Trebuchet MS"'];
           
