b8a097a4a4852c1302307910d00bd27c2d03e4c4
FKuhlewind <florian.kuhlewind@gmail.com>
2014-03-28 18:53:07 +0100

    Update code-asg1.js

--- code-asg1.js
+++ code-asg1.js
@@ -2,6 +2,19 @@ $(document).ready(function() {
 
 mainP = window.parent.document;
 
+picGallery = ["http://www.geometrian.com/data/programming/projects/Game%20of%20Life/2/GameOfLife2.png",
+            "http://blog.robbiecooper.org/wp-content/uploads/2009/02/image.jpg",
+            "http://krcadinac.com/pictures/600px-Turing_machine_1.JPG",
+             ];
+
+$("#applyPic").on("click", function(e) {
+    
+    myPicture = picGallery[$("#xselPic").val();];
+    $(mainP).find(".wrapper").css({'background-image':myPicture});
+    
+    });
+
+
 $("#yellC").on("click", function () {
     //$(mainP).find(".author").replaceWith("Suppa");
     
