6954d0865f0f098dfbce49c8f379cdfe4bc0bdbd
FKuhlewind <florian.kuhlewind@gmail.com>
2014-03-28 20:23:20 +0100

    Update code-asg1.js

--- code-asg1.js
+++ code-asg1.js
@@ -10,7 +10,7 @@ picGallery = ["url(http://www.geometrian.com/data/programming/projects/Game%20of
 // background, font, bars
 colG = [ ["red", "green", "yellow"],
          ["blue", "yellow", "black"],
-         ["#8f9d45", "white", "black"] ];
+         ["#8f9d45", "white", "#333333"] ];
              
 toColor = [ ".quote, .green, .text, .author",
             ".nav, .footer" ];
