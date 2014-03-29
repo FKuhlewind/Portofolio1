9a2eaf5748e19454f5dad3f609d6372b901d73ae
FKuhlewind <florian.kuhlewind@gmail.com>
2014-03-29 02:44:19 +0100

    Update code-asg1.js

--- code-asg1.js
+++ code-asg1.js
@@ -14,7 +14,9 @@ picGallery = ["none",
             "url(http://www.mediahosting.at/cpg15x/albums/userpics/10001/HD_Space_Wallpapers_-_Star_field_-_hubble_deep_3.jpg)",
             "url(http://i1.ytimg.com/vi/G3h1QTe9XIE/maxresdefault.jpg)",
             "url(http://www.npl.co.uk/upload/img/turing-5.jpg)",
-            "url(http://www.geekwithenvy.com/wp-content/uploads/2014/01/Aurora-.jpg)"
+            "url(http://www.geekwithenvy.com/wp-content/uploads/2014/01/Aurora-.jpg)",
+            "url(http://www.fabiovisentin.com//photography/photo/25/sky-wallpaper-02939.jpg)"
+            
             
             
              ];
