588689fb1303d4b0fbd3d7f0441c07f62f4f9a7f
FKuhlewind <florian.kuhlewind@gmail.com>
2014-03-31 22:32:14 +0200

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -74,4 +74,20 @@ localStorage.setItem("myFont", '"Trebuchet MS"');
             });
         };
     setInterval(changeText, 15000);
+    
+    //enable and disable tooltipster
+    prettyPrint();
+    $('.yell').tooltipster();
+    $('.yell').tooltipster('disable');
+       
+    $('.codeRight').on("click", ".codeButton2", function () {
+      		$('.yell').addClass('tt');
+       		$('.yell').tooltipster('enable');
+       		$('.codeButton2').replaceWith('<div class="codeButton3"><div class="dIn">Disable Tooltips</div></div>');
+       		}); 	
+      $('.codeRight').on("click", ".codeButton3", function () {
+       		$('.yell').removeClass('tt');
+       		$('.yell').tooltipster('disable');
+       		$('.codeButton3').replaceWith('<div class="codeButton2"><div class="dIn">Enable Tooltips</div></div>');
+       		}); 
 });
