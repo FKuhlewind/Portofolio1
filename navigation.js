e8e81abd651f810c93a79dae7ea4143341e47fb6
FKuhlewind <florian.kuhlewind@gmail.com>
2014-04-01 21:35:25 +0200

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -57,25 +57,7 @@ assgH = "73%";
     		} { e.preventDefault() }
     	});
    
-   //enable and disable tooltipster
-    /*prettyPrint();
-    $('.yell').tooltipster();
-    $('.yell').tooltipster('disable');
-       
-    $('.codeRight').on("click", ".codeButton2", function () {
-      		$('.yell').addClass('tt');
-       		$('.yell').tooltipster('enable');
-       		$('.codeButton2').replaceWith('<div class="codeButton3"><div class="dIn">Disable Tooltips</div></div>');
-       		}); 	
-    $('.codeRight').on("click", ".codeButton3", function () {
-       		$('.yell').removeClass('tt');
-       		$('.yell').tooltipster('disable');
-       		$('.codeButton3').replaceWith('<div class="codeButton2"><div class="dIn">Enable Tooltips</div></div>');
-       		}); */
-   
-   
    //display random quotes
-   //myDataRef = new Firebase('https://ast-quotes.firebaseio.com/');
     function changeText() {
         d3.json('https://ast-quotes.firebaseio.com/.json', function(data) {
 	    quotes = data;
@@ -98,6 +80,4 @@ assgH = "73%";
             });
         };
     setInterval(changeText, 15000);
-    
-    
 });
