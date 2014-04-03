283fe0698075dac25c7aaccc7dc508fcfe8ec3d3
FKuhlewind <florian.kuhlewind@gmail.com>
2014-04-03 11:28:26 +0200

    Update code-tool.js

--- code-tool.js
+++ code-tool.js
@@ -3,12 +3,8 @@ $(document).ready(function() {
 // check localStorage and choose font accordingly
 $('body, #toolLeft').css({'font-family':localStorage.myFont});
 $("#background, #toolRight, .carousel").css({'background-color':localStorage.myColor+'0.2)'});
+$('.carousel').css({'font-size':'15px'});
 
-//if ( !localStorage.myFont=="Trebuchet MS" ) {
-    $('.carousel').css({'font-size':'16px'});
-  //  } ;
-
-    
     $(".toolContent").hide();
     $('.carousel').on('click', function() {
         val = $(this).data('value');
