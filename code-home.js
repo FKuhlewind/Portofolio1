1ea6a362821072380cf77d524f7c23db089b7171
FKuhlewind <florian.kuhlewind@gmail.com>
2014-04-03 19:35:14 +0200

    Update code-home.js

--- code-home.js
+++ code-home.js
@@ -25,8 +25,8 @@ d3.json('https://floriansmessages.firebaseio.com/.json', function(data) {
 	    	$.each( messages , function( index, value ) {
   			    
   			    $('.appendMessage').prepend('<div class="line"></div>');
-  			    $('.appendMessage').prepend('<div class="name">'+value[name]+'</div>');
-  			    $('.appendMessage').prepend('<div class="messa">'+value[message]+'</div>');
+  			    $('.appendMessage').prepend('<div class="name">'+value.name+'</div>');
+  			    $('.appendMessage').prepend('<div class="messa">'+value.message+'</div>');
   			    $('.appendMessage .line:last-child').remove();
 			    });
             	});
