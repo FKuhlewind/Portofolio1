83156c0dcace91d74fd5a3ef4bb3ca8d415fe4c0
FKuhlewind <florian.kuhlewind@gmail.com>
2014-04-03 16:40:45 +0200

    Update code-home.js

--- code-home.js
+++ code-home.js
@@ -6,5 +6,22 @@ $("#messagebox").css({'background-color':localStorage.myColor+'0.2)'});
 $(".message").css({'background-color':localStorage.myColor+'0.3)'});
 $("button").css({'background-color':localStorage.myColor+'0.6)'});
 
+function getTime () {
+    var currentdate = new Date(); 
+    datetime = " - " + currentdate.getDate() + "."
+            + (currentdate.getMonth()+1)  + "." 
+            + currentdate.getFullYear() + ", "  
+            + currentdate.getHours() + ":"  
+            + currentdate.getMinutes() + "h"
+    };
+
+
+$("#sendMessage").on('click', function (){
+    getTime();
+  
+    alert(dateTime);
+  
+    });
+
 
 });
