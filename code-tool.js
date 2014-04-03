2c22f4fb43f29e2ac988e45fef948a24e11dfbc9
FKuhlewind <florian.kuhlewind@gmail.com>
2014-04-03 11:12:51 +0200

    Update code-tool.js

--- code-tool.js
+++ code-tool.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 // check localStorage and choose font accordingly
 $('body, #toolLeft').css({'font-family':localStorage.myFont});
 $("#background, #toolRight, .carousel").css({'background-color':localStorage.myColor+'0.2)'});
-$(".toolContent").css({'background-color':localStorage.myColor+'0.6)'});
+//$(".toolContent").css({'background-color':localStorage.myColor+'0.6)'});
     
     $(".toolContent").hide();
     $('.carousel').on('click', function() {
