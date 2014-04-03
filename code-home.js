e76720f4bf3ddfbf76315c58c3d08312f3ae50b3
FKuhlewind <florian.kuhlewind@gmail.com>
2014-04-03 16:35:50 +0200

    Update code-home.js

--- code-home.js
+++ code-home.js
@@ -2,5 +2,9 @@ $(document).ready(function() {
 
 // check localStorage and style accordingly
 $('body, #onRight').css({'font-family':localStorage.myFont});
+$("#messageBox").css({'background-color':localStorage.myColor+'0.2)'});
+$(".message").css({'background-color':localStorage.myColor+'0.3)'});
+$("button").css({'background-color':localStorage.myColor+'0.6)'});
+
 
 });
