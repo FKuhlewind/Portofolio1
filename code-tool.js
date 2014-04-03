347cf98d6748ca6bb537bfdf8e0937df1ebc6caa
FKuhlewind <florian.kuhlewind@gmail.com>
2014-04-03 11:04:25 +0200

    Update code-tool.js

--- code-tool.js
+++ code-tool.js
@@ -9,18 +9,19 @@ $("#background, #toolRight, .carousel").css({'background-color':localStorage.myC
     $('.carousel').on('click', function() {
         val = $(this).data('value');
         
-        $('.carousel').animate({'backgroundColor':localStorage.myColor+'0.2)'});
-        $(this).css({'background-color':localStorage.myColor+'0.6)'});
-        $("#toolRight").animate({'backgroundColor':localStorage.myColor+'0.6)'});
+        $('.carousel').animate({backgroundColor:localStorage.myColor+'0.2)'});
+        $(this).animate({backgroundColor:localStorage.myColor+'0.6)'});
+        $("#toolRight").animate({backgroundColor:localStorage.myColor+'0.6)'});
         
         if ( !$('#'+val).is(":visible") )
             { $(".toolContent, .hammer").slideUp("slow");
               $('#'+val).slideDown("slow"); }
         });
     $('#hammSpan').on('click', function() {
-        $('.carousel').animate({'backgroundColor':localStorage.myColor+'0.2)'});
-        $("#toolRight").animate({'backgroundColor':localStorage.myColor+'0.2)'});
+        $('.carousel').animate({backgroundColor:localStorage.myColor+'0.2)'});
+        $("#toolRight").animate({backgroundColor:localStorage.myColor+'0.2)'});
         $(".toolContent").slideUp("slow");
         $('.hammer').slideDown("slow");
         });
 });
+
