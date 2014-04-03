0b88ab33a5c49c0da09640c748aba5faafa9130a
FKuhlewind <florian.kuhlewind@gmail.com>
2014-04-03 18:57:04 +0200

    Update code-home.js

--- code-home.js
+++ code-home.js
@@ -38,8 +38,8 @@ d3.json('https://floriansmessages.firebaseio.com/.json', function(data) {
     			   addcomment = $("#messText").val();
     			   addname = nameB+' '+datetime;
     
-    			   myDataRef.child(n).name.set(addname);
-    			   myDataRef.child(n).message.set(addcomment);
+    			   myDataRef.child("floriansmessages").child(n).name.set(addname);
+    			   myDataRef.child("floriansmessages").child(n).message.set(addcomment);
     
     			   alert("I have received your message, thank you!");
     			   window.location.reload();
