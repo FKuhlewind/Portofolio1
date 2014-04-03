1da172524cdef6429d939451e0eeb52e951573b8
FKuhlewind <florian.kuhlewind@gmail.com>
2014-04-03 11:02:29 +0200

    Update code-tool.js

--- code-tool.js
+++ code-tool.js
@@ -9,7 +9,7 @@ $("#background, #toolRight, .carousel").css({'background-color':localStorage.myC
     $('.carousel').on('click', function() {
         val = $(this).data('value');
         
-        $('.carousel').animate({'background-color':localStorage.myColor+'0.2)'});
+        $('.carousel').animate({'backgroundColor':localStorage.myColor+'0.2)'});
         $(this).css({'background-color':localStorage.myColor+'0.6)'});
         $("#toolRight").animate({'background-color':localStorage.myColor+'0.6)'});
         
