30dc40d1b27dc6ba09d096f29d14633784e5e81a
FKuhlewind <florian.kuhlewind@gmail.com>
2014-04-03 08:55:23 +0200

    Update code-tool.js

--- code-tool.js
+++ code-tool.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $(".toolContent").hide()
     $('.carousel').on('click', function() {
         val = $(this).data('value');
-        $(".toolContent").slideUp("slow")
+        $(".toolContent, .hammer").slideUp("slow")
         $('#'+val).slideDown("slow");
         });
 });
