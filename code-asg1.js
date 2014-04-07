0a2b8840835e8d809703943a14e03ad8a2e695ff
FKuhlewind <florian.kuhlewind@gmail.com>
2014-04-08 00:14:18 +0200

    Update code-asg1.js

--- code-asg1.js
+++ code-asg1.js
@@ -15,7 +15,8 @@ picGallery = ["none",
             "url(http://i1.ytimg.com/vi/G3h1QTe9XIE/maxresdefault.jpg)",
             "url(http://www.npl.co.uk/upload/img/turing-5.jpg)",
             "url(http://www.geekwithenvy.com/wp-content/uploads/2014/01/Aurora-.jpg)",
-            "url(http://www.fabiovisentin.com//photography/photo/25/sky-wallpaper-02939.jpg)"
+            "url(http://www.fabiovisentin.com//photography/photo/25/sky-wallpaper-02939.jpg)",
+            "url(http://fanart.tv/fanart/music/7717539b-33aa-45d5-b88d-9e627172db19/albumcover/digital-ist-besser-50797f2451dcb.jpeg)",
              ];
 
 // content, font, bars, quote
